bb597ec429333024cc82cb5b30fa48293ccd7ab6
Miztli Melgoza <miztli.melgoza@conecta.co>
2018-01-17 22:24:38 -0600

    Add loop for system out test

--- src/main/java/com/examples/java/App.java
+++ src/main/java/com/examples/java/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println( "Hello World!" );
+        }
+
     }
 }
